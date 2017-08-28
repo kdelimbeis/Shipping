969d1df37fc829d39af3cad3d89da4013db45a5a
kostas delimbeis <kdelimbeis@comcast.net>
2017-08-28 12:28:12 -0400

    ships

--- app/controllers/ships_controller.rb
+++ app/controllers/ships_controller.rb
@@ -1,20 +1,32 @@
 class ShipsController < ApplicationController
   def index
+    @ships = Ship.all
   end
 
   def show
-    @ship = Ship.all
+    @ship = Ship.find_by_id(params[:id])
   end
 
   def create
     @user = current_user
-
-    Ship.create(name: params[:name], location: params[:location], capacity: params[:capacity], user_id: @user.id)
+    @ship = Ship.new(ship_params)
+    @ship.user_id = @user.id
+    if @ship.save!
+    # @ship = Ship.create(name: params[:name], location: params[:location], capacity: params[:capacity], user_id: @user.id)
+    end
     redirect_to "/"
   end
 
   def new
     @ship = Ship.new
   end
+
+
+  private
+
+    def ship_params
+      params.require(:ship).permit(:name, :location, :capacity, :user_id)
+    end
+
 
 end
